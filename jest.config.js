57f09783c51574eb193a179263b244b91c4cebd5
Munkhtegsh Munkhbat <munkhtegsh.m@gmail.com>
2021-09-19 11:06:56 -0700

    Add test cases and stubs

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,10 @@ const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
 
 module.exports = {
     ...jestConfig,
-    modulePathIgnorePatterns: ['<rootDir>/.localdevserver']
+    modulePathIgnorePatterns: ['<rootDir>/.localdevserver'],
+    moduleNameMapper: {
+      '^lightning/button$': '<rootDir>/force-app/test/jest-mocks/lightning/button',
+      '^thunder/hammerButton$': '<rootDir>/force-app/test/jest-mocks/thunder/hammerButton',
+      '^c/displayPanel$': '<rootDir>/force-app/test/jest-mocks/c/displayPanel'
+    }
 };
